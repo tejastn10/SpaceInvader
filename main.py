702f955d947aafa6a04e74159c5014c71f75d6c5
tejastn10 <tejastn10@gmail.com>
2020-01-11 19:57:36 +0530

    Enemy added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Initialize the pygame
 pygame.init()
@@ -16,9 +17,19 @@ playerX = 370
 playerY = 480
 playerX_change = 0
 
+# Enemy
+enemyImg = pygame.image.load('assets/images/enemy.png')
+enemyX = random.randint(0, 740)
+enemyY = random.randint(0, 100)
+enemyX_change = 0
+
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 
 running = True
@@ -48,4 +59,5 @@ while running:
         playerX = 736
 
     player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
