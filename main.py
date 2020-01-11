c6847b38d48ed595182a418e2cb8cd175dee150c
tejastn10 <tejastn10@gmail.com>
2020-01-11 15:54:02 +0530

    Added folder improvements

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ screen = pygame.display.set_mode((800, 600))
 
 # Title and Icon
 pygame.display.set_caption("Space Invaders")
-icon = pygame.image.load('ufo.png')
+icon = pygame.image.load('assets/images/ufo.png')
 pygame.display.set_icon(icon)
 
 running = True
