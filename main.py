fef3c87e61c19bb98c3b6637eb12a75c34b474c3
tejastn10 <tejastn10@gmail.com>
2020-01-11 15:49:31 +0530

    Added Title and Icon

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 
-running = True
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
 
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
